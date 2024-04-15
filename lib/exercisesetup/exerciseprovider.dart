92a878cd4996888922c91fd72bbae39e2038c644
portiktimea <portik.johanna@student.ms.sapientia.ro>
2024-04-15 18:30:43 +0300

    Implementing exercise data model and functions for saving and loading lists of Exercise objects, both for user-selected exercises and default exercises using SharedPreferences.

---
+++ lib/exercisesetup/exerciseprovider.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Exercise {
+  String name;
+  int repetitions;
+  String imagePath;
+  bool isTime;
+
+  Exercise({required this.name, required this.repetitions, required this.imagePath, required this.isTime});
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'repetitions': repetitions,
+    'imagePath': imagePath,
+    'isTime': isTime,
+  };
+
+  static Exercise fromJson(Map<String, dynamic> json) => Exercise(
+    name: json['name'],
+    repetitions: json['repetitions'],
+    imagePath: json['imagePath'],
+    isTime: json['isTime'],
+  );
+}
+
+Future<void> saveExercises(List<Exercise> exercises) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  List<String> jsonExercises = exercises.map((exercise) => json.encode(exercise.toJson())).toList();
+  await prefs.setStringList('selectedExercises', jsonExercises);
+}
+
+Future<List<Exercise>> loadExercises() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  List<String> jsonExercises = prefs.getStringList('selectedExercises') ?? [];
+  return jsonExercises.map((jsonExercise) => Exercise.fromJson(json.decode(jsonExercise))).toList();
+}
+
+Future<void> saveDefaultExercises(List<Exercise> exercises) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  List<String> jsonExercises = exercises.map((exercise) => json.encode(exercise.toJson())).toList();
+  await prefs.setStringList('defaultExercises', jsonExercises);
+}
+
+Future<List<Exercise>> loadDefaultExercises() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  List<String> jsonExercises = prefs.getStringList('defaultExercises') ?? [];
+  return jsonExercises.map((jsonExercise) => Exercise.fromJson(json.decode(jsonExercise))).toList();
+}
+
+List<Exercise> getDefaultExercises(int repetition) {
+  return [
+    Exercise(name: 'Calf Raises', repetitions: repetition, imagePath: 'lib/images/calfraises.png', isTime: false),
+    Exercise(name: 'Time', repetitions: repetition * 3, imagePath: 'lib/images/time.png', isTime: true),
+    Exercise(name: 'Toe Curls', repetitions: repetition, imagePath: 'lib/images/toecurls.png', isTime: false),
+    Exercise(name: 'Time', repetitions: repetition * 3, imagePath: 'lib/images/time.png', isTime: true),
+    Exercise(name: 'Toe Stand', repetitions: repetition * 3, imagePath: 'lib/images/toestand.png', isTime: true),
+  ];
+}
